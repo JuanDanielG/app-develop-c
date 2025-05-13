dc0d47bdf7869b7bfb70d4d2335b5d98ff218c16
JuanDanielG <jdgaleanon@ufpso.edu.co>
2025-05-13 00:35:20 -0500

    new changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-app-develop-c
+# app-develop-B
+Aplicación de control de acceso y gestión de productos
--- js/loginService.js
+++ js/loginService.js
@@ -1,70 +1,56 @@
-// Agregamos un evento 'submit' al formulario con id 'loginForm'.
-// Esto se ejecuta cuando el usuario intenta enviar el formulario.
-document.getElementById('loginForm').addEventListener('submit', function (e) {
-    // 'e.preventDefault()' evita que el formulario se envíe de forma tradicional
-    // (es decir, recargando la página).
+document.getElementById("formLogin").addEventListener('submit', function(e){
     e.preventDefault();
-    // Obtenemos los valores ingresados por el usuario en los campos de email y password.
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    console.log('valores leidos del formulario', { email, password })
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
     login(email, password)
 })
 
-// Función login que valida los datos ingresados por el usuario.
-function login(email, password) {
+function login(email, password){
     let message = ''
     let alertType = ''
-    const LOGIN_ENDPOINT = 'https://reqres.in/api/login'
-
-    fetch(LOGIN_ENDPOINT, {
-        method: 'POST',
+    localStorage.removeItem('token')
+    fetch("https://reqres.in/api/login", {
+        method: "POST",
         headers: {
-            'Content-type': 'application/json',
+            "Content-type": "application/json",
             'x-api-key': 'reqres-free-v1'
         },
         body: JSON.stringify({ email, password })
     })
-        .then((response) => {
-            return response.json().then(
-                data => {
-                    return {
-                        status: response.status,
-                        data
-                    }
-                }
-            )
-        })
-        .then((result) => {
-            if (result.status === 200) {
-                alertType = 'success'
-                message = '¡Inicio de sesión Exitoso!'
-                alertBuilder(alertType, message)
-            }
-            else {
-                alertType = 'danger'
-                message = 'Error inesperado'
-                alertBuilder(alertType, message)
-            }
-
-        })
-        .catch((error) => {
-            alertType = 'danger'
-            message = 'Error inesperado' + error
+    .then((response) =>{
+        if(response.status === 200){
+            alertType = 'success'
+            message = 'Inicio de sesión exitoso';
+            console.log('responde bien'+ response)
             alertBuilder(alertType, message)
-        })
+            response.json().then((data) => {
+                localStorage.setItem('token', data.token)
+            })            
+            setTimeout(() => {
+                location.href = 'admin/dashboard.html'
+            }, 2000) // 2000 ms = 2 segundos
+        }
+        else{
+            alertType = 'danger'
+            message = 'Correo o contraseña incorrectos.';
+            alertBuilder(alertType, message)
+        }        
+    })
+    .catch((error) => {
+        alertType = 'danger'
+        message = 'Error inesperado.';
+        console.error(error)
+        alertBuilder(alertType, message)
+    })
+}
 
-
-    // Función para generar una alerta HTML personalizada y mostrarla en la página.
-    function alertBuilder(alertType, message) {
-        // Creamos el código HTML para la alerta, usando los parámetros 'alertType' y 'message' pasados a la función.
-        const alert = `
-    <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
-        ${message}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-`
-        // Insertamos el HTML generado en el contenedor con id 'alert'.
-        document.getElementById('alert').innerHTML = alert
-    }
+function alertBuilder(alertType, message){
+    const alert = `
+        <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    `;
+    document.getElementById('alert').innerHTML = alert;
 }
